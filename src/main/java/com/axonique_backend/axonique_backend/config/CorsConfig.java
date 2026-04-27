78405cb73da0b32531c81d6c9fb5cf13502bf68b
Mitheja Manoharie <mmanoharie@gmail.com>
2026-04-27 13:46:41 +0530

    fix: Comment out redundant allowed origin entries for www.axonique.space in CORS configuration

--- src/main/java/com/axonique_backend/axonique_backend/config/CorsConfig.java
+++ src/main/java/com/axonique_backend/axonique_backend/config/CorsConfig.java
@@ -20,8 +20,8 @@ public class CorsConfig {
     config.addAllowedOriginPattern("https://*.axonique.store"); //prod
     config.addAllowedOriginPattern("https://*.vercel.app"); //vercel
     config.addAllowedOriginPattern("https://www.axonique.space");
-    config.addAllowedOrigin("https://www.axonique.space");
-    config.addAllowedOrigin("https://axonique.space");
+    // config.addAllowedOrigin("https://www.axonique.space");
+    // config.addAllowedOrigin("https://axonique.space");
     config.addAllowedMethod("*"); // Allow all HTTP methods
     config.addAllowedHeader("*"); // Allow all headers
     config.setAllowCredentials(true); 
